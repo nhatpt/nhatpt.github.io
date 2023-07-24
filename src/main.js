2bf63e4dd28ef9cf1fb48042a30c1120dad236ac
nhatpt <nhatpt@vng.com.vn>
2023-07-24 16:52:30 +0700

    feat: update CI/CD

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min"
+
 createApp(App).mount('#app')
