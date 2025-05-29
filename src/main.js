1370545309e9f737b951b376e5e62eef3f379283
Nhat. Pham Trung <37232394+nhatpt@users.noreply.github.com>
2025-05-29 14:50:16 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,5 +7,5 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min"
 
 createApp(App)
-	.use(VueReCaptcha, { siteKey: '6LcZEQ4rAAAAADljecwde_BwusdqUR0Vb167nt_w' })
+	.use(VueReCaptcha, { siteKey: '6LdBS04rAAAAAJyf4RAB7h3CvxHmnHWeSVIcyobu' })
 	.mount('#app')
